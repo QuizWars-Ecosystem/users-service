09afba83cc8941d3f1ae3c740df8f179a2b9ec81
Nofre <davidprogram417@gmail.com>
2025-04-05 18:42:39 +0200

    GitHub actions updated

---
+++ tests/integration_tests/containers/service.go
@@ -0,0 +1,10 @@
+package containers
+
+import (
+	"github.com/QuizWars-Ecosystem/users-service/tests/integration_tests/config"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+func NewServiceContainer(cfg *config.TestConfig) (testcontainers.Container, error) {
+
+}
